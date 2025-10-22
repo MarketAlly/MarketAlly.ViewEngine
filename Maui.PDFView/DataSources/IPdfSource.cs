1e7292bc5742f5928841e5246929af58d2ac689a
Dave Friedel <dave@marketally.com>
2025-10-22 05:26:53 -0400

    Fixed the viewer

--- Maui.PDFView/DataSources/IPdfSource.cs
+++
@@ -1,6 +0,0 @@
-namespace Maui.PDFView.DataSources;
-
-public interface IPdfSource
-{
-    Task<string> GetFilePathAsync();
-}
--- Maui.PDFView/PageAppearance.cs
+++
@@ -1,8 +0,0 @@
-namespace Maui.PDFView;
-
-public partial class PageAppearance
-{
-    public bool ShadowEnabled { get; set; } = true;
-    public Thickness Margin { get; set; } = new Thickness(16, 8);
-    public Thickness Crop { get; set; } = new Thickness(0);
-}
